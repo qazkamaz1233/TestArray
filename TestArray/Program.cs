41bc650409bce4d85b997c9542d359f9a0d56aab
Alexander <qazkamaz1@mail.ru>
2023-04-13 19:08:52 +0400

    Print by lines

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -10,10 +10,18 @@ namespace TestArray
     public class Program
     {
         private const int CountNum = 20;
+        private const int NumberStart = 0;
+        private const int NumberEnd = 99;
         private static readonly Random Random = new Random();
         public static void Main(string[] args)
         {
-            var array = Enumerable.Range(0, CountNum).Select(_ => Random.Next(10)).OrderBy(x => x).ToArray();
+            var array = Enumerable.Range(0, CountNum).Select(_ => Random.Next(NumberStart, NumberEnd + 1)).ToArray();
+
+
+            array = array.OrderBy(x => x).ToArray();
+
+            array.Select((value, index) => $"{index}: {value}").ForEach(Console.WriteLine);
+
 
             Console.WriteLine(String.Join(", ", array));
 
