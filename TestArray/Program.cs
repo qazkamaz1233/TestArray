c2387f1bd0dd3d8a8c840cbb03f76ed168cea050
Alexander <qazkamaz1@mail.ru>
2023-04-13 18:38:40 +0400

    update

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestArray
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const int CountNum = 20;
+        private static readonly Random Random = new Random();
+        public static void Main(string[] args)
         {
+            var array = Enumerable.Range(0, CountNum).Select(_ => Random.Next(10)).OrderBy(x => x).ToArray();
+
         }
     }
 }
